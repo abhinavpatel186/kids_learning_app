8327cda0e0d7237f78fc2406c08d5a432181780b
Patel Abhinavkumar Pramodkumar <163959810+abhinavpatel186@users.noreply.github.com>
2026-03-14 20:22:03 +0530

    Add splash screen for app startup

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            Text('Welcome to Kids Learning App!', textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
